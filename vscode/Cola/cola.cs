74c899da7b6b9e920baab4750eee4cffa99858c6
SebastiaoW93 <swillian2009@gmail.com>
2021-06-10 15:18:56 -0300

    Implementando e comentando as classes tela, Program e Posicao

---
+++ vscode/Cola/cola.cs
@@ -0,0 +1,5 @@
+
+/// <summary>Descrição geral do seu método ou classe (parte externa)(obrigatório).</summary>
+/// <param name="parametro1">Descrição geral de um parâmetro (se tiver parâmetro).</param>
+/// <param name="parametro2">Descrição geral de um parâmetro (se tiver parâmetro).</param>
+/// <returns>Retorno esperado do seu método ou classe (se tiver algum retorno).</returns>
